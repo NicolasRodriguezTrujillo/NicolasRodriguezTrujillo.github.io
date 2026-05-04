9736c65c105dbd12aef5a21b34f7a9c52550ec79
NicolasRodriguezTrujillo <ldsrmdcol@gmail.com>
2026-05-03 22:14:04 -0500

    agrego animacion

--- js/main.js
+++ js/main.js
@@ -11,3 +11,16 @@
             hamburger.classList.toggle('open');
             mobileMenu.classList.toggle('open');
         });
+
+    document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            target.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
+});
